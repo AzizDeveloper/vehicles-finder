5c4282d37237964a8f24981210f678c62210ce8c
azizdev <30.azizbek@gmail.com>
2024-03-19 16:03:47 +0500

    Initial commit

---
+++ src/main/java/dev/aziz/vehiclesfinder/VehiclesFinderApplication.java
@@ -0,0 +1,13 @@
+package dev.aziz.vehiclesfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehiclesFinderApplication.class, args);
+    }
+
+}
